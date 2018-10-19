0e1209c2ce7cab8c7d7d69d6907142c2bdc3a9ef
DogeMajor <tepsalainen@gmail.com>
2018-10-19 17:25:40 +0300

    Added methods to push and pop meshnodes at arbitrary places in the mesh

--- include/mesh.h
+++ include/mesh.h
@@ -1,111 +1,174 @@
 #ifndef MESH_H
 #define MESH_H
 #include <iostream>
-//#include "node.h"
+#include "node.h"
 #include "element.h"
+#include "Function.h"
+#include "../C++ libs/eigen/Eigen/Sparse"
+#include "../C++ libs/eigen/Eigen/Dense"
+#include "../C++ libs/eigen/Eigen/Core"
 #include <math.h>
 
 using namespace std;
 using namespace Eigen;
 
 
+template <typename Nodetype>
+struct MeshNode {
+	Nodetype data;
+	MeshNode<Nodetype>* next;
+};
+
+
 template <int Dim, int N, typename T>
-class Mesh{
+class Mesh : Counter<Mesh<Dim, N, T> > {
 
 public:
-    Mesh();
-    Mesh(Element<Dim,N,T> &t);
-    Mesh(Element<Dim,N,T> t, Mesh<Dim,N,T> *n);
-    ~Mesh();
-    const Mesh <Dim,N,T>& operator=(const Mesh<Dim,N,T> &m);
-    bool operator==(const Mesh<Dim,N,T> &m) const;
-    bool operator!=(const Mesh<Dim,N,T> &m) const;
-    Element<Dim,N,T> get_element() const;
-    const Mesh<Dim,N,T>* get_next();
-    double weak_form_element(int i, int j) const;//Calculating the stiffness matrix
-    void show() const;
+	Mesh();
+	Mesh(Element<Dim, N, T> &t);
+	~Mesh();
+	bool push(Element<Dim, N, T> &t);//To the top  OK
+	bool pop();//From the top  OK
+	bool push(MeshNode<Element<Dim, N, T> > *previous, Element<Dim, N, T> &t);//Adds efter the previous lement!
+	bool pop(MeshNode<Element<Dim, N, T> > *previous);//Deletes the next element!!!
+	int how_many() const;// { return objects_alive; }
+	int how_many_nodes() const;
+	//vector <T> divide_element(T &el);
+	MeshNode<Element<Dim, N, T> > * get_top_mesh_node() { return top; }
+	const Element<Dim, N, T> get_top() { return top->data; }
+	const Element<Dim, N, T> get_last();
+	void refine();
+	bool operator==(const Mesh<Dim, N, T> & m) const;
+	bool operator!=(const Mesh<Dim, N, T> & m) const;
+	void show() const;
 
 private:
-    Element <Dim,N,T> top;
-    Mesh<Dim,N,T> *next;
+	MeshNode<Element<Dim, N, T> > *top;
+	ElementDivider<Dim, N, T> divider;
+	int node_counter;
 
 };
 
 template <int Dim, int N, typename T>
-Mesh<Dim,N,T>::Mesh(){
-    //top = new Element<Dim,N,T>;
-    //top = new Element<Dim,N,T>;
-    next = nullptr;
+Mesh<Dim, N, T>::Mesh() {
+	top = nullptr;
+	node_counter = 0;
 }
 
 template <int Dim, int N, typename T>
-Mesh<Dim,N,T>::Mesh(Element<Dim,N,T> &t){
-    top = t;
-    next = nullptr;
-}
-
-
-template <int Dim, int N, typename T>
-Mesh<Dim,N,T>::Mesh(Element<Dim,N,T> t, Mesh<Dim,N,T> *n){
-    top = t;
-    next = n;
+Mesh<Dim, N, T>::Mesh(Element<Dim, N, T> &t) {
+	top = new MeshNode <Element<Dim, N, T> >{ t, nullptr };
+	node_counter = 1;
 }
 
 template <int Dim, int N, typename T>
-Mesh<Dim,N,T>::~Mesh(){
-    //delete top;
-    //delete next;
-    delete next;
-    cout << "Mesh destroyed!" << endl;
+Mesh<Dim, N, T>::~Mesh() {
+	while (pop() != false) {}
+	cout << "Mesh destroyed!" << endl;
+	cout << "Nodes left in the mesh: " << node_counter << endl;
 }
 
 template <int Dim, int N, typename T>
-const Mesh <Dim,N,T>& Mesh<Dim,N,T>::operator=(const Mesh<Dim,N,T> & m){
-    if(*this!=m){
-        top = m.get_element();
-        next = m.get_next();
-    }
-    return *this;
+bool Mesh<Dim, N, T>::push(Element<Dim, N, T> &t) {
+	top = new MeshNode <Element<Dim, N, T> >{ t, top };
+	node_counter++;
+	return true;
 }
 
 template <int Dim, int N, typename T>
-bool Mesh<Dim,N,T>::operator==(const Mesh<Dim,N,T> & m) const{
-    bool same_top = (m.top == top);
-    bool same_next = (m.next == next);
-    return same_top && same_next;
+bool Mesh<Dim, N, T>::pop() {
+	if (top != nullptr) {
+		MeshNode<Element<Dim, N, T> > temp = *top;
+		delete top;
+		top = temp.next;
+		node_counter--;
+		return true;
+	}
+	return false;
 }
 
 template <int Dim, int N, typename T>
-bool Mesh<Dim,N,T>::operator!=(const Mesh<Dim,N,T> & m) const{
-    return !(*this == m);
+bool Mesh<Dim, N, T>::pop(MeshNode<Element<Dim, N, T> > *previous) {
+	if (previous->next != nullptr && previous != nullptr) {
+		MeshNode<Element<Dim, N, T> >* temp = previous->next;
+		previous->next = temp->next;
+		delete temp;
+		node_counter--;
+		return true;
+	}
+	return false;
 }
 
 template <int Dim, int N, typename T>
-Element<Dim,N,T> Mesh<Dim,N,T>::get_element() const{
-    return top;
+bool Mesh<Dim, N, T>::push(MeshNode<Element<Dim, N, T> > *previous, Element<Dim, N, T> &t) {
+	MeshNode <Element<Dim, N, T> >* temp = previous->next;
+	previous->next = new MeshNode <Element<Dim, N, T> >{ t, temp };
+	node_counter++;
+	return true;
 }
 
 template <int Dim, int N, typename T>
-const Mesh<Dim,N,T>* Mesh<Dim,N,T>::get_next(){
-    return next;
+int Mesh<Dim, N, T>::how_many() const {
+	return objects_alive;
 }
 
 template <int Dim, int N, typename T>
-double Mesh<Dim,N,T>::weak_form_element(int i, int j) const{
-    return 0.0;
+int Mesh<Dim, N, T>::how_many_nodes() const {
+	return node_counter;
 }
 
 template <int Dim, int N, typename T>
-void Mesh<Dim,N,T>::show() const{
-    //Mesh<Dim,N,T>* next_mesh = get_next();
-    cout << "To be fixed..." << endl;
-    top.show();
-    /*do{
-        top.show();
-        this = next;
-    }
-    while(this != nullptr);*/
+bool Mesh<Dim, N, T>::operator==(const Mesh<Dim, N, T> & m) const {
+	bool same_top = (m.top == top);
+	return same_top;
 }
+
+template <int Dim, int N, typename T>
+bool Mesh<Dim, N, T>::operator!=(const Mesh<Dim, N, T> & m) const {
+	return !(*this == m);
+}
+
+template <int Dim, int N, typename T>
+const Element<Dim,N,T> Mesh<Dim, N,T>::get_last() {
+	if (top->next == nullptr) { return top->data; }
+	MeshNode <Element<Dim, N, T> >* iter = top;
+	while (iter->next != nullptr) { iter = iter->next; }
+	return iter->data;
+}
+
+template <int Dim, int N, typename T>
+void Mesh<Dim, N, T>::refine() {
+	MeshNode<Element<Dim, N, T> >* previous = top;//For future needs...
+
+	MeshNode<Element<Dim, N, T> >* iter = top->next;;//For future needs...
+	vector<Element<Dim, N, T>* > new_els = divider.divide(top->data);
+	pop();//Delete the first node - the next old node has to be deleted with some other method
+	push(*new_els[0]);
+	previous = top;
+	for (int i = 1; i < new_els.size(); i++) {
+		push(*new_els[i]);
+	}
+	//while (iter != nullptr) {
+		//new_els = divider.divide(iter->data);
+	//}
+
+}
+
+template <int Dim, int N, typename T>
+void Mesh<Dim, N, T>::show() const {
+	//BaseMesh<int, N T>* next_BaseMesh = get_next();
+	cout << "Amount of nodes: " << node_counter << endl;
+	MeshNode<Element<Dim, N, T> >* iter = top;
+	int count = 0;
+	while (iter != nullptr) {
+		cout << "Showing elemnt no " << count << endl;
+		iter->data.show();
+		iter = iter->next;
+		count++;
+	}
+}
+
+
 
 #endif
 
--- tests/testMesh.cpp
+++ tests/testMesh.cpp
@@ -1,7 +1,7 @@
 #include "../include/point.h"
 #include "../include/element.h"
 #include "../include/mesh.h"
-#include "../include/LinkedMesh.h"
+//#include "../include/LinkedMesh.h"
 
 #include <math.h>
 
@@ -44,7 +44,7 @@ TEST_CASE( "Test Element template containing Node template initiated with 2-D do
     //second_mesh.show();
 */
 
-TEST_CASE("Test LinkedMesh with Elements based on Points") {
+/*TEST_CASE("Test LinkedMesh with Elements based on Points") {
 
 	vector <double> vec1 = { 0.0, 0.0 };
 	vector <double> vec2 = { 1.0, 0.0 };
@@ -149,9 +149,141 @@ TEST_CASE("Test LinkedMesh with Elements based on Points") {
 		double volume = mesh.get_element_volume(el);
 		REQUIRE( volume == 1.0 );
 	}
+
+
+}*/
+
+
+TEST_CASE("Test the real Mesh with Elements based on Points") {
+
+	vector <double> vec1 = { 0.0, 0.0 };
+	vector <double> vec2 = { 1.0, 0.0 };
+	vector <double> vec3 = { 1.0, 1.0 };
+	vector <double> vec4 = { 0.0, 1.0 };
+	Point <double> point1(vec1);
+	Point <double> point2(vec2);
+	Point <double> point3(vec3);
+	Point <double> point4(vec4);
+	Node <2, Point <double> > n_1(point1);
+	Node <2, Point <double> > n_2(point2);
+	Node <2, Point <double> > n_3(point3);
+	vector<Node <2, Point <double>  > * > node_vec(3, nullptr);
+	node_vec[0] = new Node<2, Point <double> >(point1);
+	node_vec[1] = new Node<2, Point <double> >(point2);
+	node_vec[2] = new Node<2, Point <double> >(point3);
+	ElementFactory<2, 3, Point <double> > factory;
+	Element<2, 3, Point <double> > el1 = factory.build(node_vec);
+	vector<Node <2, Point <double> > *> node_vec2;//(3, nullptr);
+	node_vec2.push_back(node_vec[0]);
+	node_vec2.push_back(node_vec[2]);
+	node_vec2.push_back(new Node<2, Point <double> >(point4));
+	Element<2, 3, Point <double> > el2 = factory.build(node_vec2);
+	Mesh<2, 3, Point <double> > el_mesh(el1);
+	el_mesh.push(el2);
+	//el_mesh.show();
+	//el_mesh.get_top().show();
+
+	//LinkedMesh< Node<2, Point <double> > > node_mesh(*node_vec[0]);//ok
+
+
+	SECTION("Mesh can be initialized with default constructor") {//OK
+		Mesh<2, 3, Point <double> > empty_mesh;
+		REQUIRE(empty_mesh.how_many() == 2);
+	}
+
+	SECTION("One can access top element") {//OK
+		REQUIRE(el_mesh.get_top() == el2);
+	}
+
+	SECTION("One can access last element") {//OK
+		REQUIRE(el_mesh.get_last() == el1);
+	}
+
+	SECTION("One can pop and push at the top") {//OK
+		REQUIRE(el_mesh.pop());
+		REQUIRE(el_mesh.get_top() == el1);
+		REQUIRE(el_mesh.push(el2));
+		REQUIRE(el_mesh.get_top() == el2);
+		REQUIRE(el_mesh.get_last() == el1);
+	}
+
+	SECTION("One can pop a MeshNode from anywhere in the Mesh") {//OK!!
+		MeshNode<Element<2, 3, Point <double> > >* top_mesh_node = el_mesh.get_top_mesh_node();
+		REQUIRE(el_mesh.pop(top_mesh_node));
+		REQUIRE(el_mesh.get_last() == el2);
+		top_mesh_node = el_mesh.get_top_mesh_node();
+		REQUIRE(el_mesh.pop(top_mesh_node) == false);
+		//Rebuild the mesh!!
+		REQUIRE(el_mesh.pop());
+		REQUIRE(el_mesh.push(el1));
+		REQUIRE(el_mesh.push(el2));
+		REQUIRE(el_mesh.how_many_nodes() == 2);
+		REQUIRE(el_mesh.get_last() == el1);
+	}
+
+	SECTION("One can push a MeshNode from anywhere in the Mesh") {//OK!!
+		MeshNode<Element<2, 3, Point <double> > >* top_mesh_node = el_mesh.get_top_mesh_node();
+		REQUIRE(el_mesh.push(top_mesh_node, el2));//Should be pushed at the middle place
+		el_mesh.show();
+
+		REQUIRE(el_mesh.how_many_nodes() == 3);
+		REQUIRE(el_mesh.get_last() == el1);
+		//Rebuild the mesh!!
+		REQUIRE(el_mesh.pop(top_mesh_node) == true);
+		REQUIRE(el_mesh.how_many_nodes() == 2);
+	}
+
+	SECTION("Refining the mesh should succeed") {
+		el_mesh.refine();
+		REQUIRE(el_mesh.how_many_nodes() == 5);
+		vector<double> new_loc1 = { 0, 0 };
+		//el_mesh.show();
+		//REQUIRE(el_mesh.get_top()[0].get_location() == new_loc1);
+	}
+
+	//SECTION( "Test copy constructors" ){
+		//LinkedMesh < Element <2, 3, Point <double> > > el_mesh2(el_mesh);
+		//REQUIRE( el_mesh2.get_top() == el_mesh.get_top() );
+		//REQUIRE( el_mesh2.get_last() == el_mesh.get_last());
+	//}
+	/*
+	SECTION( "Test operators == and !=" ){
+		BaseMesh < Element <2, 3, Point <double> > > similar_mesh(el);
+		BaseMesh < Element <2, 3, Point <double> > > mesh3;
+		REQUIRE( mesh1 == similar_mesh );
+		REQUIRE( mesh1 == mesh1 );
+		REQUIRE( mesh3 != mesh1 );
+	}
+
+	SECTION( "Test get_element" ){
+		REQUIRE( mesh1.get_element() == el );
+	}
+
+	SECTION( "Test get_next" ){
+		REQUIRE( mesh1.get_next() == nullptr );
+	}
+
+	SECTION( "Test set_top" ){
+		BaseMesh < Element <2, 3, Point <double> > > empty_mesh;
+		empty_mesh.set_top(el);
+		REQUIRE( mesh1.get_element() == el );
+	}
+
+/*
+	SECTION( "Test get_simplex_matrix" ){
+		Matrix<double,2,2> mat = mesh.get_simplex_matrix(el);
+		cout << mat << endl;
+		REQUIRE( mat == MatrixXd::Identity(2,2) );
+	}
+
+	SECTION( "Test get_element_volume" ){
+		double volume = mesh.get_element_volume(el);
+		REQUIRE( volume == 1.0 );
+	}
 */
 
 }
+
 
 
 
